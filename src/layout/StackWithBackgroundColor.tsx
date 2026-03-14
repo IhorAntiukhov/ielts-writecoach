ea524278ee9f301b925d158823f36faca048eac9
IhorAntiukhov <holaborun@gmail.com>
2026-03-14 12:31:25 +0200

    Fix page titles and routing

--- src/layout/StackWithBackgroundColor.tsx
+++ src/layout/StackWithBackgroundColor.tsx
@@ -10,6 +10,7 @@ export default function StackWithBackgroundColor() {
           contentStyle: {
             backgroundColor: "tranparent",
           },
+          title: "IELTS WriteCoach",
         }}
       />
     </View>
